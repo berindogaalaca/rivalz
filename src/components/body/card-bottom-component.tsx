aeebb69846a4dfb39f6bc134e7ec2779ff4ab5aa
Berin <dogaalacaaa@gmail.com>
2024-06-12 12:23:42 +0300

    change

--- src/components/body/card-bottom-component.tsx
+++ src/components/body/card-bottom-component.tsx
@@ -7,7 +7,7 @@ const CardBottomComponent: React.FC = () => {
   return (
     <div className="container mx-auto mb-28">
       <div className="flex flex-col justify-center items-start mt-14">
-        <h2 className="text-header text-xl font-bold mb-4">RIVALZ ORIGINALS</h2>
+        <h2 className="text-header text-xl  px-8 font-bold mb-4">RIVALZ ORIGINALS</h2>
       </div>
       <div className="container mx-auto flex custom-scroll px-8  py-4 items-center ">
         <div className="flex  space-x-24">
