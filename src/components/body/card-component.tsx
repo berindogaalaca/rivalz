2abb198d9e74ad2afc228a24ca1e70befc3b1eec
Berin Doğa Alaca <73670917+berindogaalaca@users.noreply.github.com>
2024-06-12 17:49:46 +0300

    update homepage cards

--- src/components/body/card-component.tsx
+++ src/components/body/card-component.tsx
@@ -4,7 +4,7 @@ import { cardData } from "@/dummy/dummy";
 
 const CardComponent = () => {
   return (
-    <div className="container mx-auto mt-40 md:mt-40 flex flex-col items-center md:grid md:grid-cols-2 md:gap-4 md:justify-between lg:grid-cols-3 xl:grid-cols-4">
+    <div className="container mx-auto mt-40 md:mt-40 flex flex-col items-center md:grid md:grid-cols-2  md:justify-between lg:grid-cols-3 xl:grid-cols-4">
       {cardData.map((card, index) => (
         <div key={index} className="mb-3 md:mb-0 mx-6">
           <Card
